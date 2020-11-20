29907e29f26271d8a7566811a63dfb2d5fb0ef28
zassou65535 <gravity@kuf.biglobe.ne.jp>
2020-11-20 11:54:37 +0900

    adaILNに関する説明を追加

--- module/base_module.py
+++ module/base_module.py
@@ -43,6 +43,9 @@ class ResnetAdaILNBlock(nn.Module):
         out = self.norm2(out, gamma, beta)
         return out + x
 
+#入力されたTensorに対し
+#各チャネル別々に正規化したものと、そのレイヤー内でいっぺんに正規化をかけたもの
+#双方を割合rhoで混ぜ合わせ出力とする層
 class adaILN(nn.Module):
     def __init__(self, num_features, eps=1e-5):
         super(adaILN,self).__init__()
@@ -51,13 +54,15 @@ class adaILN(nn.Module):
         self.rho.data.fill_(0.9)
 
     def forward(self, input, gamma, beta):
-        #各チャネルの特徴マップに対し、
-        #各チャネルごとに、特徴マップの縦横全ての平均と分散を計算
+        #各チャネル別々に、特徴マップに対し正規化をかける
+        #各チャネル別々に、特徴マップの縦横全ての平均と分散を計算
         in_mean, in_var = torch.mean(input, dim=[2, 3], keepdim=True), torch.var(input, dim=[2, 3], keepdim=True)
         out_in = (input - in_mean) / torch.sqrt(in_var + self.eps)
+        #レイヤーの全部の特徴マップに対し正規化をかける
         #特徴マップのチャネル＋縦横全ての平均と分散を計算（そのレイヤーの全部の値に対して計算）
         ln_mean, ln_var = torch.mean(input, dim=[1, 2, 3], keepdim=True), torch.var(input, dim=[1, 2, 3], keepdim=True)
         out_ln = (input - ln_mean) / torch.sqrt(ln_var + self.eps)
+        #self.rhoを割合としてout_inとout_lnを混ぜ合わせ出力とする
         out = self.rho.expand(input.shape[0], -1, -1, -1) * out_in + (1-self.rho.expand(input.shape[0], -1, -1, -1)) * out_ln
         out = out * gamma.unsqueeze(2).unsqueeze(3) + beta.unsqueeze(2).unsqueeze(3)
         return out
